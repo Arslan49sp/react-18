4d711ecfa0c3c802e0383b14ba208f50f86dc40b
Arslan Majeed <arslanmajeed214@gmail.com>
2024-10-09 01:26:58 +0500

    passing functions via props.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import Message from "./Message";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  const items = [
-    "Lahore",
-    "Karachi",
-    "Islamabad",
-    "Rawalpindi",
-    "Faisalabad",
-    "Mardan",
-  ];
+  const items = ["Lahore", "Karachi", "Islamabad", "Rawalpindi", "Faisalabad"];
+  const handleSelectItem = (item: string) => console.log(item);
   return (
     <div>
-      <ListGroup items={items} heading="cities" />
+      <ListGroup
+        items={items}
+        heading="cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
