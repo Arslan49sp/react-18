a11551f8b33526d90ef742763354e383df3c1a43
Arslan Majeed <arslanmajeed214@gmail.com>
2024-10-22 15:01:33 +0500

    complete the exercise 3 of updating state object.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheeze"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
   return (
     <div>
-      {pizza.name}
+      {cart.discount}
       <ul>
-        {pizza.toppings.map((topping) => (
-          <li key={topping}>{topping}</li>
+        {cart.items.map((item) => (
+          <li key={item.id}>
+            {item.title} {item.quantity}
+          </li>
         ))}
       </ul>
-      <button onClick={handleClick}>Add</button>
+      <button onClick={handleClick}>+</button>
     </div>
   );
 }
