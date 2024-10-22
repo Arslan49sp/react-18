6b290afaf7a3da4abba880acc67ab96eed273752
Arslan Majeed <arslanmajeed214@gmail.com>
2024-10-22 16:06:48 +0500

    create the form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { useState } from "react";
-import ExpandedText from "./components/ExpandedText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandedText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-        nesciunt, praesentium, delectus amet veniam ratione iure soluta impedit
-        esse ipsam voluptatem facere sed, distinctio ex veritatis cum fuga
-        dolorum explicabo provident harum autem est velit. Aliquid tempore
-        consequuntur dignissimos, quisquam molestiae dolores nulla iusto, natus
-        mollitia in quasi provident consequatur nemo officiis perspiciatis sed
-        optio architecto! Porro officiis qui eos repudiandae dolores repellendus
-        quibusdam amet ex culpa iste ullam, fuga ratione sunt facere dolorum
-        nihil! Odit est officia commodi aliquam sit cumque, ullam dicta corporis
-        delectus iusto vitae? Tempore soluta nostrum saepe temporibus vero
-        doloremque, distinctio accusantium quos quia perspiciatis?
-      </ExpandedText>
+      <Form />
     </div>
   );
 }
