ed1f914184a8a6b1b7cd5cd52b2e10b20f6f1c7e
Arslan Majeed <arslanmajeed214@gmail.com>
2024-10-24 18:53:53 +0500

    understand the useEffect dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
 import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import ProductList from "./components/ProductList";
 
 function App() {
-  const [changeCategory, setChangeCategory] = useState("");
-  const [expenses, setExpenses] = useState([
-    { id: 2, description: "Milk", amount: 10, category: "Groceries" },
-  ]);
-
-  const visibleExpenses = changeCategory
-    ? expenses.filter((e) => e.category === changeCategory)
-    : expenses;
+  const [category, setCategoy] = useState("");
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter onSelect={(category) => setChangeCategory(category)} />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
-      />
+      <select
+        className="form-select w-50"
+        onChange={(event) => setCategoy(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Laundery">Laundering</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 }
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+interface Props {
+  category: string;
+}
+const ProductList = ({ category }: Props) => {
+  const [products, setProducts] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log("Fetching the products in " + category);
+    setProducts(["chaddi", "bunyaan"]);
+  }, [category]);
+
+  return <div>ProductList</div>;
+};
+
+export default ProductList;
