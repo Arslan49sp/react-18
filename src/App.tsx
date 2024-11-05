9dcd0ef03d31d66cce15da1f7c2de256e1285bf7
Arslan Majeed <arslanmajeed214@gmail.com>
2024-11-05 10:53:51 +0500

    update users

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,22 @@ function App() {
         setUsers(origionalUser);
       });
   };
+
+  const updateUser = (user: User) => {
+    const origionalUser = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    axios
+      .patch(
+        "https://jsonplaceholder.typicode.com/users/" + user.id,
+        updatedUser
+      )
+      .catch((err) => {
+        setError(err.message);
+        setUsers(origionalUser);
+      });
+  };
+
   const deleteUser = (user: User) => {
     const origionalUser = [...users];
     setUsers(users.filter((u) => u.id !== user.id));
@@ -64,12 +80,20 @@ function App() {
             key={user.id}
           >
             {user.name}
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => deleteUser(user)}
-            >
-              Delete
-            </button>
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
