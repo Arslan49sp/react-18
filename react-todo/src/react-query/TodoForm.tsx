14580814008753673bd5b2f89b91bd867f1f0607
Arslan Majeed <arslanmajeed214@gmail.com>
2024-11-12 15:41:06 +0500

    optimistic update of the newly added data in UI

--- react-todo/src/react-query/TodoForm.tsx
+++ react-todo/src/react-query/TodoForm.tsx
@@ -3,29 +3,47 @@ import axios from "axios";
 import { useRef } from "react";
 import { Todo } from "../hooks/useTodos";
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
 
+    onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        newTodo,
+        ...(todos || []),
+      ]);
+      if (ref.current) ref.current.value = "";
+      return { previousTodos };
+    },
+
     onSuccess: (savedTodo, newTodo) => {
-      //APPROACH 1: invalidating cache.
+      // APPROACH 1: invalidating cache.
       // But we cannot use it because it is not supported in JsonPlaceHolder as it is a fake API
-      //it will not return the newly added todo.
-      //
+      // it will not return the newly added todo.
+
       // queryClient.invalidateQueries({
       //   queryKey: ['todos']
       // });
 
       // APPROACH 2:  Updating the cache directly.
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        savedTodo,
-        ...(todos || []),
-      ]);
-      if (ref.current) ref.current.value = "";
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+
+    onError: (error, todo, context) => {
+      if (!context) return;
+
+      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
     },
   });
   const ref = useRef<HTMLInputElement>(null);
