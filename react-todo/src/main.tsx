64becd1f847a553f7b4097454051762a31303a10
Arslan Majeed <arslanmajeed214@gmail.com>
2025-02-13 13:31:42 +0500

    set static routes

--- react-todo/src/main.tsx
+++ react-todo/src/main.tsx
@@ -1,16 +1,17 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
+import routes from "./routing/routes";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={routes} />
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
